7ec9002ed146bbd90a316769ba9ce1560eac213f
Alex <alex.smoura7@gmail.com>
2020-03-23 00:36:27 -0300

    Configuração do DataBinding no gradle Criação do primeiro Fragment

---
+++ app/src/main/java/com/alex/quizmatematica/TitleFragment.kt
@@ -0,0 +1,33 @@
+package com.alex.quizmatematica
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import com.alex.quizmatematica.databinding.FragmentTitleBinding
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class TitleFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        val binding: FragmentTitleBinding = DataBindingUtil.inflate(
+                inflater, R.layout.fragment_title, container, false)
+
+        return binding.root
+
+
+        }
+    }
+
+
+}
